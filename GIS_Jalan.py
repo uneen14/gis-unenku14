68e883bd6a846d337661d275d772e1d7087a7955
uneen14 <76991847+uneen14@users.noreply.github.com>
2026-04-04 01:31:51 +0700

    Refactor GIS_Jalan.py for improved structure

--- GIS_Jalan.py
+++ GIS_Jalan.py
@@ -61,7 +61,7 @@ with st.sidebar:
     uploaded_file = st.file_uploader("Unggah GeoJSON Baru", type=["geojson", "json"])
 
 # Tampilkan Judul dengan Class CSS yang sudah dibuat
-st.markdown(f'<h2 class="judul-jalan">📍 {nama_jalan_input}</h2>', unsafe_allow_html=True)
+st.markdown(f'<h1 class="judul-jalan">📍 {nama_jalan_input}</h1>', unsafe_allow_html=True)
 
 # ==========================================
 # 5. PEMROSESAN DATA
